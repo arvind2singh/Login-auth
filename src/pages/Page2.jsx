64fe463396639cc24441975af9460a984b180853
Arvind Singh <arvindsingh224u@gmail.com>
2021-10-09 01:12:37 +0530

    commit

--- src/pages/Page2.jsx
+++ src/pages/Page2.jsx
@@ -1,19 +1,35 @@
-import React from 'react'
+import React from "react";
 
 import { useDispatch } from "react-redux";
-import {user_logout} from './redux/authaction';
+import { user_logout } from "./redux/authaction";
 
 const Page2 = (props) => {
-    
-    const dispatch = useDispatch();
-    return (
-        <>
-            <h1>Goodbye World</h1>  
-            
-            <button type="button" className="btn btn-info color-3" style={{margin:"10px"}} onClick={(e) => {props.history.push("/home");}}>Home</button>
-            <button type="button" className="btn btn-danger color-3" onClick={(e) => {dispatch(user_logout(() => document.location.reload()));}}>Logout</button>
-        </>
-    )
-}
+  const dispatch = useDispatch();
+  return (
+    <>
+      <h1>Goodbye World</h1>
 
-export default Page2
+      <button
+        type="button"
+        className="btn btn-info color-3"
+        style={{ margin: "10px" }}
+        onClick={(e) => {
+          props.history.push("/home");
+        }}
+      >
+        Home
+      </button>
+      <button
+        type="button"
+        className="btn btn-danger color-3"
+        onClick={(e) => {
+          dispatch(user_logout(() => props.history.push("/login")));
+        }}
+      >
+        Logout
+      </button>
+    </>
+  );
+};
+
+export default Page2;
